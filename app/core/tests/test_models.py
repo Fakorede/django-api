d746ec53ca03de063543e4c1f1b6d8fa1412a4e9
Fakorede <abiolafakorede@gmail.com>
2020-05-24 14:39:23 +0100

    implemented normalize email addresses using TDD

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,10 @@ class ModelTests(TestCase):
 
 		self.assertEqual(user.email, email)
 		self.assertTrue(user.check_password(password))
+
+	def test_new_user_email_normalized(self):
+		"""The email for a new user is normalized"""
+		email = "abiolafakorede@GMAIL.COM"
+		user = get_user_model().objects.create_user(email, '12345')
+
+		self.assertEqual(user.email, email.lower())
